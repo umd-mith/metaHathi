b528de5b148b5b38785523c8e3dae9ba5647dbc4
raffazizzi <raffaeleviglianti@gmail.com>
2014-08-06 17:08:37 -0400

    separated post and get request builder; closing client on import completion; started working on project client

--- src/main/scala/org/mith/metaHathi/utils/openRefine.scala
+++ src/main/scala/org/mith/metaHathi/utils/openRefine.scala
@@ -11,32 +11,84 @@ import java.io.ByteArrayInputStream
 import java.util.ArrayList
 
 import org.apache.http.entity.mime.MultipartEntityBuilder
-import org.apache.http.client.methods.HttpPost
+import org.apache.http.client.methods.{HttpPost, HttpGet}
 
 import org.apache.http.impl.client.HttpClientBuilder
 import org.apache.http.util.EntityUtils
 
+object HTTPUtils {
 
-class OpenRefineImporter(refineHost:String) {
+  private def encode(params:Map[String, String]) : String = {
+    val p = params.view map {
+      case (k, v) => k + "=" + v
+    } mkString ("", "&", "")
+    "?" + p
+  }
 
-  val client = HttpClientBuilder.create().build  
-
-  def createRequest(command:String, params:Map[String, String] = null) : HttpPost = {
+  def createPostRequest(refineHost:String, command:String, params:Map[String, String] = null) : HttpPost = {
 
     val paramStr = if (params != null) {
-      val p = params.view map {
-        case (k, v) => k + "=" + v
-      } mkString ("", "&", "")
-      "?" + p
+      encode(params)
     } else ""
+
+    println("http://" + refineHost + "/" + command + paramStr)
 
     new HttpPost("http://" + refineHost + "/" + command + paramStr)
   }
 
+  def createGetRequest(refineHost:String, command:String, params:Map[String, String] = null) : HttpGet = {
+
+    val paramStr = if (params != null) {
+      encode(params)
+    } else ""
+
+    new HttpGet("http://" + refineHost + "/" + command + paramStr)
+  }
+
+}
+
+class OpenRefineProjectClient(refineHost:String, projectId:String, refineData:String = "/tmp/refine"){
+
+  val client = HttpClientBuilder.create().build    
+
+  // Get history of operations (ignoring future ones)
+  // command/core/get-history
+  // return list of past operation ids
+  def getHistory() = {
+    import org.apache.http.NameValuePair
+    import org.apache.http.client.entity.UrlEncodedFormEntity
+    import org.apache.http.message.BasicNameValuePair
+
+    val request = HTTPUtils.createGetRequest(refineHost, "command/core/get-history", Map("project" -> projectId))
+
+    val response = client.execute(request)
+
+    EntityUtils.toString(response.getEntity())
+
+  }
+
+  // get hathi id for row
+
+  // get column name and infer hierarchy 
+  // command/core/get-models
+
+  // get changes for operation id
+  // this goes to the refineData folder, unzips the right project file, reads change data for operation id
+  // Builds bare JSON object
+  // Or it could build full object
+
+}
+
+
+
+class OpenRefineImporter(refineHost:String) {
+
+  val client = HttpClientBuilder.create().build    
+
   def createImportingJob() : String = {
     // Create job      
 
-    val request = createRequest("command/core/create-importing-job")
+    val request = HTTPUtils.createPostRequest(refineHost, "command/core/create-importing-job")
     request.addHeader("Content-Type", "application/json")
 
     val response = client.execute(request)
@@ -49,7 +101,7 @@ class OpenRefineImporter(refineHost:String) {
   def checkStatus (jobId:String) = {
     // Check job status
 
-    val request = createRequest("command/core/get-importing-job-status", Map("jobID" -> jobId)) 
+    val request = HTTPUtils.createPostRequest(refineHost, "command/core/get-importing-job-status", Map("jobID" -> jobId)) 
 
     val response = client.execute(request)
     EntityUtils.toString(response.getEntity())
@@ -67,11 +119,14 @@ class OpenRefineImporter(refineHost:String) {
                      "jobID" -> jobId,
                      "subCommand" -> "load-raw-data")
 
-    val request = createRequest("command/core/importing-controller", params)
+    val request = HTTPUtils.createPostRequest(refineHost, "command/core/importing-controller", params)
 
     val entity = MultipartEntityBuilder.create()
 
+    // val out = new java.io.PrintWriter(new java.io.FileWriter("/tmp/full.json"))
+
     for ( d <- data ) {
+      // out.println(d.toString)
       val dataStream = new ByteArrayInputStream(d.toString.getBytes(StandardCharsets.UTF_8))
       entity.addBinaryBody("f", dataStream, ContentType APPLICATION_JSON, "f")
     }
@@ -94,8 +149,10 @@ class OpenRefineImporter(refineHost:String) {
         def getAsyncProjectId() : String = {
           val status: Json = Parse.parseOption(checkStatus(jobId)).get  
           val cursor = status.hcursor
-          val state = (cursor --\ "job" --\ "config" --\ "projectID")
-          state.focus.getOrElse( getAsyncProjectId() ).toString
+          val pid = (cursor --\ "job" --\ "config" --\ "projectID")
+          val value = pid.focus.getOrElse( getAsyncProjectId() ).toString
+          client.close()
+          value
         }
         
         getAsyncProjectId()
@@ -131,7 +188,7 @@ class OpenRefineImporter(refineHost:String) {
         "subCommand" -> "create-project"
       )
 
-    val request = createRequest("command/core/importing-controller", params)
+    val request = HTTPUtils.createPostRequest(refineHost, "command/core/importing-controller", params)
 
     val nameValuePairs = new ArrayList[NameValuePair](1)
     nameValuePairs.add(new BasicNameValuePair("format", "text/json"))
